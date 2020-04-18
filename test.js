aa799a17e2c8f1436c4fdb297ba92b7e0958366b
yudai <yudai.0702wa@gmail.com>
2020-04-19 02:07:54 +0900

    音声出力変更できるようにした

--- test.js
+++
@@ -1,8 +0,0 @@
-function doSomethingLoop(maxCount, i) {
-  if (i <= maxCount) {
-    console.log("あ")
-    setTimeout(function(){doSomethingLoop(maxCount, ++i)}, 1000);
-  }
-}
-
-doSomethingLoop(10, 0);
