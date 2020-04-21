869c0a16d9413b65873187ab7ba212b1b86ee82d
yudai <yudai.0702wa@gmail.com>
2020-04-21 20:49:56 +0900

    配列を修正

--- naokiroid.js
+++ naokiroid.js
@@ -12,21 +12,28 @@ $(document).ready(function () {
         let list = "./mp3/naokiroid/"+res[i]+".mp3";
 
         if (res[i] == "ー"){
+
+          console.log(res)
+
+          let x = i - 1;
+
+          console.log(res[x]);
           
-          if(res[i-1] == "あ" || "か" || "さ" || "た" || "な" || "は" || "ま" || "や" || "ら" || "わ"){
+          if(res[x] == "あ" || "か" || "さ" || "た" || "な" || "は" || "ま" || "や" || "ら" || "わ"){
             list = "./mp3/naokiroid/あー.mp3";
-          }else if(res[i-1] == "い" || "き" || "し" || "ち" || "に" || "ひ" || "み" || "い" || "り"){
+          }else if(res[x] == "い" || "き" || "し" || "ち" || "に" || "ひ" || "み" || "い" || "り"){
             list = "./mp3/naokiroid/いー.mp3";
-          }else if(res[i-1] == "う" || "く" || "す" || "つ" || "ぬ" || "ふ" || "む" || "ゆ" || "る"){
+          }else if(res[x] == "う" || "く" || "す" || "つ" || "ぬ" || "ふ" || "む" || "ゆ" || "る"){
             list = "./mp3/naokiroid/うー.mp3";
-          }else if(res[i-1] == "え" || "け" || "せ" || "て" || "ね" || "へ" || "め" || "え" || "れ"){
+          }else if(res[x] == "え" || "け" || "せ" || "て" || "ね" || "へ" || "め" || "え" || "れ"){
             list = "./mp3/naokiroid/えー.mp3";
-          }else if(res[i-1] == "お" || "こ" || "そ" || "と" || "の" || "ほ" || "も" || "よ" || "ろ" || "を"){
+          }else if(res[x] == "お" || "こ" || "そ" || "と" || "の" || "ほ" || "も" || "よ" || "ろ" || "を"){
             list = "./mp3/naokiroid/おー.mp3";
           }
         }
         
-        audioElem.src = list;  
+        audioElem.src = list; 
+        console.log(list); 
         audioElem.play();
         setTimeout(function(){doSomethingLoop(maxCount, ++i)}, textspeed);
       }
---
+++ test.js
@@ -0,0 +1,7 @@
+<script src="lib/jquery-3.5.0.min.js"></script>
+
+let inputText = $("#Text1").val(いーうえお);
+
+let res = inputText.split("");
+
+console.log(res);
